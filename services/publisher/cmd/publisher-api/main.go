eaa804228fe0d5412dd5a0cd5d256a58f9ae6655
VikasKumar1187 <vikasdhaka.nitk@gmail.com>
2024-09-01 17:47:30 +0530

    5

--- services/publisher/cmd/publisher-api/main.go
+++ services/publisher/cmd/publisher-api/main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"context"
+	"errors"
+	"expvar"
 	"fmt"
 	"os"
+	"runtime"
+	"time"
 
-	"github.com/vikaskumar1187/saas-project/services/publisher/pkg/logger"
+	"github.com/ardanlabs/conf/v3"
+	"github.com/yourusername/saas-project/services/publisher/pkg/logger"
+	"go.uber.org/zap"
 )
 
 var build = "develop"
@@ -17,21 +23,100 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-
 	defer log.Sync()
 
-	if err := run(log); err != nil {
+	ctx := context.Background()
+
+	if err := run(ctx, log); err != nil {
 		log.Errorw("startup", "ERROR", err)
 		log.Sync()
 		os.Exit(1)
 	}
 
+}
+
+func run(ctx context.Context, log *zap.SugaredLogger) error {
 	// -------------------------------------------------------------------------
+	// GOMAXPROCS
 
-	ctx := context.Background()
+	log.Infow("startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
 
-	if err := run(ctx, log); err != nil {
-		log.Error(ctx, "startup", "msg", err)
-		return
+	// -------------------------------------------------------------------------
+	// Configuration
+
+	cfg := struct {
+		conf.Version
+		Web struct {
+			ReadTimeout     time.Duration `conf:"default:5s"`
+			WriteTimeout    time.Duration `conf:"default:10s"`
+			IdleTimeout     time.Duration `conf:"default:120s"`
+			ShutdownTimeout time.Duration `conf:"default:20s"`
+			APIHost         string        `conf:"default:0.0.0.0:3000"`
+			DebugHost       string        `conf:"default:0.0.0.0:4000"`
+		}
+
+		DB struct {
+			User         string `conf:"default:postgres"`
+			Password     string `conf:"default:postgres,mask"`
+			Host         string `conf:"default:database-service.publisher-system.svc.cluster.local"`
+			Name         string `conf:"default:postgres"`
+			MaxIdleConns int    `conf:"default:2"`
+			MaxOpenConns int    `conf:"default:0"`
+			DisableTLS   bool   `conf:"default:true"`
+		}
+	}{
+		Version: conf.Version{
+			Build: build,
+			Desc:  "Vikas K",
+		},
 	}
+
+	const prefix = "PUBLISHER"
+	help, err := conf.Parse(prefix, &cfg)
+	if err != nil {
+		if errors.Is(err, conf.ErrHelpWanted) {
+			fmt.Println(help)
+			return nil
+		}
+		return fmt.Errorf("parsing config: %w", err)
+	}
+
+	// -------------------------------------------------------------------------
+	// App Starting
+
+	log.Info(ctx, "starting service", "version", build)
+	defer log.Info(ctx, "shutdown complete")
+
+	out, err := conf.String(&cfg)
+	if err != nil {
+		return fmt.Errorf("generating config for output: %w", err)
+	}
+	log.Info(ctx, "startup", "config", out)
+
+	expvar.NewString("build").Set(build)
+
+	// -------------------------------------------------------------------------
+	// Database Support
+
+	log.Info(ctx, "startup", "status", "initializing database support", "host", cfg.DB.Host)
+
+	db, err := db.Open(db.Config{
+		User:         cfg.DB.User,
+		Password:     cfg.DB.Password,
+		Host:         cfg.DB.Host,
+		Name:         cfg.DB.Name,
+		MaxIdleConns: cfg.DB.MaxIdleConns,
+		MaxOpenConns: cfg.DB.MaxOpenConns,
+		DisableTLS:   cfg.DB.DisableTLS,
+	})
+	if err != nil {
+		return fmt.Errorf("connecting to db: %w", err)
+	}
+	defer func() {
+		log.Info(ctx, "shutdown", "status", "stopping database support", "host", cfg.DB.Host)
+		db.Close()
+	}()
+
+	return nil
+
 }
